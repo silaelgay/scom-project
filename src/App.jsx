848df08cf94e3380dd9b40e90179ff5814bcf076
silaelgay <elgaysla488@gmail.com>
2025-02-11 19:14:33 +0300

    dependency güncellemesi

--- src/App.jsx
+++ src/App.jsx
@@ -33,3 +33,18 @@ function App() {
 }
 
 export default App
+
+import React from 'react';
+
+  import { ToastContainer, toast } from 'react-toastify';
+  
+  function App(){
+    const notify = () => toast("Wow so easy!");
+
+    return (
+      <div>
+        <button onClick={notify}>Notify!</button>
+        <ToastContainer />
+      </div>
+    );
+  }
